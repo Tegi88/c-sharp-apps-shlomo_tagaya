4f8b4bc7677e0f047d0907b45e7f4733a8ca68b6
Shlomo Tagaya <tagaya88@gmail.com>
2024-05-24 09:15:22 +0300

    chang to public

--- c-sharp-apps-shlomo_tagaya/Program.cs
+++ c-sharp-apps-shlomo_tagaya/Program.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace c_sharp_apps_shlomo_tagaya
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
